7c4154d7a88829698286280e23868f33f2012619
Sudhir Takale <sudhirtakale99@gmail.com>
2024-04-21 08:07:35 +0530

    refactored winner evaluator class

--- src/main/java/com/amaap/cleanstrike/domain/service/WinnerEvaluator.java
+++ src/main/java/com/amaap/cleanstrike/domain/service/WinnerEvaluator.java
@@ -4,7 +4,6 @@ import com.amaap.cleanstrike.domain.model.entity.CaromBoard;
 import com.amaap.cleanstrike.domain.model.entity.Player;
 import com.amaap.cleanstrike.domain.service.outcomeprocesses.*;
 import com.amaap.cleanstrike.service.PlayerService;
-
 import com.google.inject.Inject;
 
 import java.util.ArrayList;
@@ -12,58 +11,63 @@ import java.util.List;
 
 public class WinnerEvaluator {
     private final PlayerService playerService;
+    private boolean isGameOver = false;
 
     @Inject
     public WinnerEvaluator(PlayerService playerService) {
         this.playerService = playerService;
     }
 
-
-    public String getWinner(CaromBoard caromBoard) {
-        int MAX_SCORE_TO_WIN = 5;
-        int MIN_SCORE_TO_WIN = 3;
-
+    private List<CaromBoardState> initializeBoardStates() {
         List<CaromBoardState> boardStates = new ArrayList<>();
         boardStates.add(new RedStrikeProcessor());
         boardStates.add(new MultiStrikeProcessor());
         boardStates.add(new StrikeProcessor());
         boardStates.add(new DefunctCoinProcessor());
         boardStates.add(new StrikerStrikeProcessor());
+        return boardStates;
+    }
 
+    public String getWinner(CaromBoard caromBoard) {
+        List<CaromBoardState> boardStates = initializeBoardStates();
         Player firstPlayer = caromBoard.getPlayers().get(0);
         Player secondPlayer = caromBoard.getPlayers().get(1);
 
-        while (true) {
-
-            int randomIndex = playerService.getRandomIndex(boardStates.size());
-            CaromBoardState selectedState = boardStates.get(randomIndex);
-
-            selectedState.applyStrike(caromBoard, firstPlayer);
+        while (!isGameOver) {
+            applyRandomStrike(boardStates, caromBoard, firstPlayer);
             System.out.println("Player one points :" + firstPlayer.getPoints());
 
-            int newRandomIndex = playerService.getRandomIndex(boardStates.size());
-            CaromBoardState newSelectedState = boardStates.get(newRandomIndex);
+            String result = checkScore(firstPlayer, secondPlayer);
+            if (!result.equals("Draw")) return result;
 
-            newSelectedState.applyStrike(caromBoard, secondPlayer);
+            applyRandomStrike(boardStates, caromBoard, secondPlayer);
             System.out.println("Player two points :" + secondPlayer.getPoints());
 
-
-            if (caromBoard.getBlackCoins() == 0 && caromBoard.getRedCoins() == 0) {
-                System.out.println("Draw");
-                return "Draw";
-            } else if (firstPlayer.getPoints() >= MAX_SCORE_TO_WIN && firstPlayer.getPoints() - secondPlayer.getPoints() >= MIN_SCORE_TO_WIN) {
-                System.out.println(caromBoard.getBlackCoins() + " " + caromBoard.getRedCoins());
-                System.out.println("firstPlayer");
-                return "Player 1";
-
-            } else if (secondPlayer.getPoints() >= MAX_SCORE_TO_WIN && secondPlayer.getPoints() - firstPlayer.getPoints() >= MIN_SCORE_TO_WIN) {
-                System.out.println(caromBoard.getBlackCoins() + " " + caromBoard.getRedCoins());
-                System.out.println("secondPlayer");
-                return "Player 2";
-
-
-            }
+            result = checkScore(firstPlayer, secondPlayer);
+            if (!result.equals("Draw")) return result;
         }
+        return "Draw";
+    }
 
+
+    private void applyRandomStrike(List<CaromBoardState> boardStates, CaromBoard caromBoard, Player player) {
+        int randomIndex = playerService.getRandomIndex(boardStates.size());
+        CaromBoardState selectedState = boardStates.get(randomIndex);
+        selectedState.applyStrike(caromBoard, player);
+    }
+
+    private String checkScore(Player firstPlayer, Player secondPlayer) {
+        int MAX_SCORE_TO_WIN = 5;
+        int MIN_SCORE_TO_WIN = 3;
+
+        if (firstPlayer.getPoints() >= MAX_SCORE_TO_WIN && firstPlayer.getPoints() - secondPlayer.getPoints() >= MIN_SCORE_TO_WIN) {
+            System.out.println("Player 1 wins by " + firstPlayer.getPoints() + " to " + secondPlayer.getPoints());
+            isGameOver = true;
+            return "Player 1";
+        } else if (secondPlayer.getPoints() >= MAX_SCORE_TO_WIN && secondPlayer.getPoints() - firstPlayer.getPoints() >= MIN_SCORE_TO_WIN) {
+            System.out.println("Player 2 wins by " + secondPlayer.getPoints() + " to " + firstPlayer.getPoints());
+            isGameOver = true;
+            return "Player 2";
+        } else return "Draw";
     }
 }
